5bdd5eafa75a4d129281db9797c7fc34ce5f0339
HARFHO <Harfho77@gmail.com>
2022-03-11 15:22:30 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -7,9 +7,10 @@
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtcView.user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtcView.user.js
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchycorpPtcView.user.js
-// @include      https://autofaucet.dutchycorp.space/ptc/view.php*
-// @include      https://autofaucet.dutchycorp.space/ptc/index.php
-// @include      https://autofaucet.dutchycorp.space/ptc
+// @include      *autofaucet.dutchycorp.space/ptc/view.php*
+// @include      *autofaucet.dutchycorp.space/ptc/index.php
+// @include      *autofaucet.dutchycorp.space/ptc/
+// @include      *autofaucet.dutchycorp.space/ptc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dutchycorp.space
 // @grant        none
 // ==/UserScript==
