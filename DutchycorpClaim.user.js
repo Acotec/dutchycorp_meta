48e2b58be98ab0dce8703b14b804be15abee0908
HARFHO <Harfho77@gmail.com>
2022-03-15 11:14:24 +0100

    Update DutchycorpClaim.user.js

--- DutchycorpClaim.user.js
+++ DutchycorpClaim.user.js
@@ -1,7 +1,6 @@
-// ==UserScript==
 // @name         DutchycorpClaim
 // @namespace    https://github.com/Acotec/dutchycorp
-// @version      1.01
+// @version      1.1
 // @description  AutomateButtons
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpClaim.user.js
@@ -12,13 +11,14 @@
 // @connect      api.github.com
 // @resource     _DontOpen https://gist.githubusercontent.com/Harfho/493dc66ecebd58a75b730a77ef676632/raw/_DontOpen.txt
 // @resource     shortlinks_name https://gist.githubusercontent.com/Harfho/493dc66ecebd58a75b730a77ef676632/raw/shortlinks_name.txt
-// @include       *autofaucet.dutchycorp.space/shortlinks-wall*
+// @include      *autofaucet.dutchycorp.space/shortlinks-wall*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_getResourceText
 // @grant        unsafeWindow
 // @grant        window.close
 // @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
 // @noframes
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchycorpClaim.user.js
 // ==/UserScript==
