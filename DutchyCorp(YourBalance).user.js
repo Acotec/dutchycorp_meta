fd5871839f823b046ff5da1c76032de2a6cdec1f
HARFHO <Harfho77@gmail.com>
2022-03-21 14:33:02 +0100

    Update DutchyCorp(YourBalance).user.js

--- DutchyCorp(YourBalance).user.js
+++ DutchyCorp(YourBalance).user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         DutchyCorp(YourBalance)
 // @namespace    Acot
-// @version      0.1.1
+// @version      0.1.2
 // @description  Scroll to Coin to withdraw
 // @author       Acot
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(YourBalance).user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(YourBalance).user.js
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchyCorp(YourBalance).user.js
-// @match        https://autofaucet.dutchycorp.space/your_balance.php
+// @match        https://autofaucet.dutchycorp.space/your_balance.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dutchycorp.space
 // ==/UserScript==
 (function(){})();
