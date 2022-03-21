1b8c2cef62824d61044795bf8a426824679afe47
HARFHO <Harfho77@gmail.com>
2022-03-21 14:29:13 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name         DutchycorpPtcView
 // @namespace    Acot
-// @version      0.2
+// @version      0.3
 // @description  click Submit when countdown end, when viewing ptc
 // @author       Acot
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtcView.user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtcView.user.js
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchycorpPtcView.user.js
-// @include      *autofaucet.dutchycorp.space/ptc/view.php*
-// @include      *autofaucet.dutchycorp.space/ptc/index.php
-// @include      *autofaucet.dutchycorp.space/ptc/
-// @include      *autofaucet.dutchycorp.space/ptc
+// @include      *autofaucet.dutchycorp.space/ptc*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dutchycorp.space
 // @grant        none
 // ==/UserScript==
