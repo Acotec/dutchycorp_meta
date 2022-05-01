5b9eaa15ac63ea04260e3542ed9305db13bbcd4d
HARFHO <Harfho77@gmail.com>
2022-05-01 09:34:16 +0100

    Create PageLoaded.user.js

---
+++ PageLoaded.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         PageLoaded
+// @namespace    Acotec
+// @version      0.1
+// @description  Add loaded to page title when page load
+// @author       Acot
+// @match       *://*/*
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/PageLoaded.user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/PageLoaded.user.js
+// @require      https://github.com/Acotec/dutchycorp_script/raw/master/PageLoaded.user.js
+// @run-at       document.start
+// ==/UserScript==
+(function(){})();
