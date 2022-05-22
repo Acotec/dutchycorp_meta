03e971e54d6e166dfa0b82d20a961a086c7f266c
HARFHO <Harfho77@gmail.com>
2022-05-22 22:03:07 +0100

    Create ReloadShortlinkPage.user.js

---
+++ ReloadShortlinkPage.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         ReloadShortlinkPage
+// @namespace    Acotec
+// @version      0.1.1
+// @description  Refresh page after every 1minute for 3time and stop
+// @author       Acotec
+// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/ReloadShortlinkPage.user.js
+// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/ReloadShortlinkPage.user.js
+// @match        *://*/*
+// @grant        GM_addElement
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM_getValue
+// @grant        window.close
+// @run-at       document-start
+// @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(ReloadShortlinkPage).user.js
+// ==/UserScript==
+(function(){})();
