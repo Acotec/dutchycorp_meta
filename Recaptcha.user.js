b98a30ec8c68d1cc2e700e4408c8b946a2d37f01
HARFHO <Harfho77@gmail.com>
2022-06-05 08:58:34 +0100

    Update Recaptcha.user.js

--- Recaptcha.user.js
+++ Recaptcha.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Recaptcha Solver
 // @namespace    Acotec
-// @version      2.2
+// @version      2.3
 // @description  Recaptcha Solver in Browser
 // @author       Acot (credit to engageub)
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/Recaptcha.user.js
@@ -10,5 +10,8 @@
 // @connect      engageub.pythonanywhere.com
 // @connect      engageub1.pythonanywhere.com
 // @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @require      https://github.com/Acotec/autofcb_script/raw/master/Recaptcha.user.js
 // ==/UserScript==
