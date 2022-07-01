9432cd2f816550c1362ba1975a00678981a305b9
HARFHO <Harfho77@gmail.com>
2022-07-01 08:53:38 +0100

    Update and rename Dutchycorp_ip.user.js to Dutchycorp_ipBs.user.js

---
+++ Dutchycorp_ipBs.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Dutchycorp_ipBs
+// @namespace    Acot
+// @version      0.2
+// @description  Reload page when dutchycorp says a user is already usin the ip
+// @author       Acot
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/Dutchycorp_ipBs.user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/Dutchycorp_ipBs.user.js
+// @require      https://github.com/Acotec/dutchycorp_script/raw/master/Dutchycorp_ipBs.user.js
+// @include      *autofaucet.dutchycorp.space*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @run-at       document-start
+// @noframes
+// ==/UserScript==
+(function() {})();
