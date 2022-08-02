72446d9bcc77697bfad4d8bc1de716a2943f89c1
HARFHO <Harfho77@gmail.com>
2022-08-03 00:16:54 +0100

    Update Dutchycorp_claimDone_close.user.js

--- Dutchycorp_claimDone_close.user.js
+++ Dutchycorp_claimDone_close.user.js
@@ -1,16 +1,21 @@
 // ==UserScript==
 // @name         Dutchycorp_claimDone_close
 // @namespace    https://github.com/Acotec/Dutchycorp
-// @version      0.2
+// @version      0.3
 // @description  close dutchcorp when done
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/Dutchycorp_claimDone_close.user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/Dutchycorp_claimDone_close.user.js
 // @include      *autofaucet.dutchycorp.space/shortlinks-wall.php?*key=*
 // @include      *autofaucet.dutchycorp.space/shortlinks-wall.php*already_done*
+// @include      *shortlinks-wall.php*
 // @icon         https://www.google.com/s2/favicons?domain=dutchycorp.space
 // @run-at       document-start
 // @grant        window.close
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @noframes
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/Dutchycorp_claimDone_close.user.js
 // ==/UserScript==
 (function() {})();
