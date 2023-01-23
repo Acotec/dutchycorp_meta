9e20927bfa9ad9995e858e83fa2eefc4e0b135fa
HARFHO <Harfho77@gmail.com>
2023-01-23 02:50:19 +0100

    Update DutchycorpPtc.user.js

--- DutchycorpPtc.user.js
+++ DutchycorpPtc.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         DutchycorpPtc
 // @namespace    Acot
-// @version      0.2.1
+// @version      0.2.2
 // @description  view ptc and close when done 
 // @author       Acotec
 // @match        https://autofaucet.dutchycorp.space/ptc/wall.php*
-// @require      https://greasyfork.org/scripts/12228/code/setMutationHandler.js
+//// @require      https://greasyfork.org/scripts/12228/code/setMutationHandler.js
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtc.user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchycorpPtc.user.js
 // @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchycorpPtc.user.js
