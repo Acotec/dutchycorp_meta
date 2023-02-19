0142c0643b33f669549b76385b3c689640b1e208
HARFHO <Harfho77@gmail.com>
2023-02-19 01:30:15 +0100

    Create DutchyCorp(BOOSTEDROLL).user.js

---
+++ DutchyCorp(BOOSTEDROLL).user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         DutchyCorp(BOOSTEDROLL)
+// @version      0.1
+// @description  BOOSTED ROLL
+// @author       Acotec
+// @match        https://autofaucet.dutchycorp.space/coin_roll.php*
+// @match        https://autofaucet.dutchycorp.space/roll.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dutchycorp.space
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(BOOSTEDROLL).user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(BOOSTEDROLL).user.js
+// @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchyCorp(BOOSTEDROLL).user.js
+// @grant        none
+// ==/UserScript==
+(function() {})();
