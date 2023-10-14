faf4a591d528d193bd08daa9d8edb69e2c3d5c33
HARFHO <39716223+Harfho@users.noreply.github.com>
2023-10-14 15:43:30 +0100

    Create DutchyCorp(FastWithdrawn).user.js

---
+++ DutchyCorp(FastWithdrawn).user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         DutchyCorp(FastWithdrawn)
+// @namespace    http://tampermonkey.net/
+// @description   withdraw
+// @version       0.2
+// @author        Acotec
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(FastWithdrawn).user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/DutchyCorp(FastWithdrawn).user.js
+// @require      https://github.com/Acotec/dutchycorp_script/raw/master/DutchyCorp(FastWithdrawn).user.js
+// @match        https://autofaucet.dutchycorp.space/exchange.php*
+// @match        https://autofaucet.dutchycorp.space/your_balance.php*
+// @connect      autofaucet.dutchycorp.space
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dutchycorp.space
+// @require      https://greasyfork.org/scripts/461948-fbase-lib/code/FBase%20Lib.js?version=1224586
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// ==/UserScript==
