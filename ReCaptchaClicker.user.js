dffe146176c9c2a7afc1384cb07a53d57f7e7caf
HARFHO <39716223+Harfho@users.noreply.github.com>
2023-12-23 02:48:20 +0100

    Update ReCaptchaClicker.user.js

--- ReCaptchaClicker.user.js
+++ ReCaptchaClicker.user.js
@@ -6,8 +6,8 @@
 // @author       Acot (credit to satology)
 // @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/master/ReCaptchaClicker.user.js
 // @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/master/ReCaptchaClicker.user.js
-// @require      https://github.com/Acotec/dutchycorp_script/raw/master/ReCaptchaClicker.user.js
 // @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @require      https://github.com/Acotec/dutchycorp_script/raw/master/ReCaptchaClicker.user.js
 // @match        https://autofaucet.dutchycorp.space/*
 // @match        *://*/recaptcha/api2/anchor*
 // @grant        GM_setValue
