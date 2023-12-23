3b8180086deab313d30641e496e3aed7eb06d60a
HARFHO <39716223+Harfho@users.noreply.github.com>
2023-12-23 02:37:17 +0100

    Create ReCaptchaClicker.user.js

---
+++ ReCaptchaClicker.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         ReCaptchaClicker
+// @namespace    Acotec
+// @version      2.5
+// @description  Recaptcha Solver in Browser
+// @author       Acot (credit to satology)
+// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/ReCaptchaClicker.user.js
+// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/ReCaptchaClickeruser.js
+// @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @match        https://autofaucet.dutchycorp.space/*
+// @match        *://*/recaptcha/api2/anchor*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
+// ==/UserScript==
