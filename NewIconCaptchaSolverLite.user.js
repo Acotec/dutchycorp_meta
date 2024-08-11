442a9025e070246071badb1fa021b959190551e8
HARFHO <39716223+Harfho@users.noreply.github.com>
2024-08-11 05:43:43 +0100

    Create  NewIconCaptchaSolverLite.user.js

---
+++ NewIconCaptchaSolverLite.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         New IconCaptcha Solver Lite
+// @namespace    New Icon Captcha Solver
+// @version      0.6
+// @description  Solves New Icon Captcha
+// @author       faucetTimeSaver
+// @match        https://earnbitmoon.club/*
+// @match        https://rsadnetworkinfo.com/*
+// @match        https://rseducationinfo.com/*
+// @match        https://rshostinginfo.com/*
+// @match        https://rsfinanceinfo.com/*
+// @match        https://rsinsuranceinfo.com/*
+// @match        https://rssoftwareinfo.com/*
+// @match        https://autofaucet.dutchycorp.space/*
+// @require      https://unpkg.com/jimp@0.5.2/browser/lib/jimp.min.js
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/root/NewIconCaptchaSolverLite.user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/root/NewIconCaptchaSolverLite.user.js
+// @require      https://github.com/Acotec/dutchycorp_script/raw/root/NewIconCaptchaSolverLite.user.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
