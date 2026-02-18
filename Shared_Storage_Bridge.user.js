29472a07ee3745ba50a5b046a3c31aae7a67bfa3
HARFHO <39716223+Harfho@users.noreply.github.com>
2026-02-18 13:44:36 +0100

    Update version and description in user script

---
+++ Shared_Storage_Bridge.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Shared_Storage_Bridge
+// @namespace    https://github.com/Acotec
+// @version      0.1
+// @description  Shared_Storage_Bridge script for the extension
+// @author       Acotec
+// @updateURL    https://github.com/Acotec/dutchycorp_meta/raw/root/Shared_Storage_Bridge.user.js
+// @downloadURL  https://github.com/Acotec/dutchycorp_meta/raw/root/Shared_Storage_Bridge.user.js
+// @connect      gist.github.com
+// @connect      gist.githubusercontent.com
+// @connect      api.github.com
+// @match        // @match *://*/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_getResourceText
+// @grant        unsafeWindow
+// @grant        window.close
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @noframes
+// @require      https://github.com/Acotec/dutchycorp_script/raw/root/Shared_Storage_Bridge.user.js
+// ==/UserScript==
+(function () {})();
